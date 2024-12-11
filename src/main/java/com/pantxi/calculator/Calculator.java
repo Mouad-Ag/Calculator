d2f1e4421cd762de7c8ef63cd1b0c024cf95f1cc
Mouad-Ag <mouadag10@gmail.com>
2024-12-11 20:52:18 +0100

    quatrieme commit

--- src/main/java/com/pantxi/calculator/Calculator.java
+++ src/main/java/com/pantxi/calculator/Calculator.java
@@ -1,10 +1,14 @@
 package com.pantxi.calculator;
 
 public class Calculator {
-    public int add(int a, int b) {
+
+    // Méthode addition statique
+    public static int add(int a, int b) {
         return a + b;
     }
-    public int divide(int opG, int opD){
-        return opG/opD;
+
+    // Méthode division statique
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
--- src/test/java/com/pantxi/calculator/CalculatorTest.java
+++ src/test/java/com/pantxi/calculator/CalculatorTest.java
@@ -1,34 +1,19 @@
 package com.pantxi.calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
-
-    @BeforeEach
-    public void setUp() {
-        calculator = new Calculator();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        calculator = null;
-    }
-
     @Test
     public void testAddition() {
-        int result = calculator.add(2, 3);
+        int result = Calculator.add(2, 3);
         assertEquals(5, result, "2 + 3 doit être égal à 5");
     }
 
     @Test
     public void testDivision() {
-        int result = calculator.divide(6, 3);
+        int result = Calculator.divide(6, 3);
         assertEquals(2, result, "6 / 3 doit être égal à 2");
     }
 }
-
