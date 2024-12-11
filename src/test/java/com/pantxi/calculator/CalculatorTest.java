add1f501d4120a3cf12e973bd2f3db4e1a83335c
Mouad-Ag <mouadag10@gmail.com>
2024-12-11 20:24:04 +0100

    troisieme commit

--- src/test/java/com/pantxi/calculator/CalculatorTest.java
+++ src/test/java/com/pantxi/calculator/CalculatorTest.java
@@ -1,18 +1,34 @@
 package com.pantxi.calculator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        calculator = null;
+    }
+
     @Test
     public void testAddition() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result, "2 + 3 doit être égal à 5");
     }
+
     @Test
     public void testDivision() {
-        Calculator calculator = new Calculator();
         int result = calculator.divide(6, 3);
         assertEquals(2, result, "6 / 3 doit être égal à 2");
     }
 }
+
